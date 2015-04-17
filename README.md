02970b98d9977dbb01ca2bc25fb5906ecc3ac3c8
sole <sole@mozilla.com>
2015-04-17 11:28:19 +0100

    a bit more complete template

--- README.md
+++ README.md
@@ -19,3 +19,9 @@ npm run build
 ```
 
 Demo files will be placed in `build/`.
+
+Remember to rebuild the bundle each time you make a change to the demo or node code. Alternatively you can also run the watch task, so it will watch for file changes and then rebuild the bundle for you:
+
+```bash
+npm run watch
+```
--- demo/main.js
+++ demo/main.js
@@ -1,11 +1,18 @@
-var TemplateNode = require('../');
+require('openmusic-oscilloscope').register('openmusic-oscilloscope');
 
 var ac = new AudioContext();
 var limiter = ac.createGain();
 limiter.gain.value = 0.25;
 limiter.connect(ac.destination);
 
+var analyser = ac.createAnalyser();
+analyser.connect(limiter);
+
+var oscilloscope = document.querySelector('openmusic-oscilloscope');
+oscilloscope.attachTo(analyser);
+
+// Change below depending on what your audio node needs to do:
+var TemplateNode = require('../');
 var node = TemplateNode(ac);
-node.connect(limiter);
-
-
+node.connect(analyser);
+node.start();
